51ad9c806de3a0c432314a6b15ce90ac3138b7e7
Dehghan <Dehghan@DESKTOP-H75T4QA>
2025-07-10 13:34:40 +0330

    projects section from index page moved to partial view Projects

---
+++ Solution1/DevCard_MVC/Models/IndexModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class IndexModel
+    {
+        public List<Project> Projects  { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
