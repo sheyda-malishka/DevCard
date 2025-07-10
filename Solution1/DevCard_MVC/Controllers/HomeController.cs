b86cae68e4437afc57a293126e50068cf1c18712
Dehghan <Dehghan@DESKTOP-H75T4QA>
2025-07-10 11:07:18 +0330

    Index & Contact page added

--- Solution1/DevCard_MVC/Controllers/HomeController.cs
+++ Solution1/DevCard_MVC/Controllers/HomeController.cs
@@ -6,11 +6,10 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
@@ -18,10 +17,12 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
