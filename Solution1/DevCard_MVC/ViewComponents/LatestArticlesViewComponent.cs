18edafc3927f4ac43396ab66dcd63750a3d87214
Dehghan <Dehghan@DESKTOP-H75T4QA>
2025-07-12 11:33:27 +0330

    Projects $ Latest Articles Moved to View Component

---
+++ Solution1/DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invok()
+        {
+            var articles = new List<Article> 
+            { 
+                new Article(id:1 , title:"آموزش asp.net core mvc" , description:"کامل ترین پکیج آموزش asp.net core mvc" , image:"blog-post-thumb-card-1.jpg"),
+                new Article(id:2 , title:"آموزش Git & Github" , description:"کامل ترین پکیج آموزش Git & Github" , image:"blog-post-thumb-card-2.jpg"),
+                new Article(id:3 , title:"آموزش Onion Architecture" , description:"کامل ترین پکیج آموزش Onion Architecture" , image:"blog-post-thumb-card-3.jpg"),
+                new Article(id:4 , title:"آموزش Front-end" , description:"کامل ترین پکیج آموزش Front-end" , image:"blog-post-thumb-card-4.jpg"),
+            };
+
+            return View("_LatestArticles" , articles);
+        }   
+    }
+}
---
+++ Solution1/DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project> 
+            { 
+                new Project(id:1 , name:"تاکسی " , description:"درخواست انلاین تاکسی",image:"project-1.jpg" , client:"Atriya"),
+                new Project(id:2 , name:"زودفود" , description:"درخواست انلاین غذا ", image:"project-2.jpg",client:"ZoodFood"),
+                new Project(id:3 , name:"مدارس  " , description:"سیستم یکپارجه مدارس" ,image:"project-3.jpg", client:"MONOP"),
+                new Project(id:4 , name:"فضاپیما " , description:"برنامه مدیریت فضاپیماهای ناسا",image:"project-4.jpg" , client:"NASA"),
+            };
+            
+            
+
+            return View("_Projects" , projects);
+        }
+
+    }
+}
